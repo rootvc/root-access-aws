66699c21c42c78c0be6f9efb337c4b17346c07aa
Lee Edwards <leeredwards@gmail.com>
2021-12-07 15:12:01 -1000

    Process Nylas message

--- .aws-sam/build/SNSPayloadLogger/src/handlers/sns-nylas-message.js
+++ .aws-sam/build/SNSPayloadLogger/src/handlers/sns-nylas-message.js
@@ -1,8 +1,51 @@
-/**
- * A Lambda function that logs the payload received from SNS.
- */
+handleEmail = (snsMessage) => {
+    let data = {};
+    for (let index in Object.keys(snsMessage.data)) {
+        const key = Object.keys(snsMessage.data)[index];
+        // console.info('(' + index + ') ' + key + ': ' +snsMessage.data[key]);
+        data[key] = snsMessage.data[key];
+    }
+
+    // commented out fields are valid, but not used
+    const message = {
+        "bcc": protectNull(data.bcc),
+        // "body": data.body,
+        "cc": protectNull(data.cc),
+        "date": data.date, // TODO: format
+        // "folder": data.folder,
+        "from": protectNull(data.from),
+        "grant_id": data.grant_id,
+        "id": data.id,
+        // "object": data.object,
+        "reply_to": protectNull(data.reply_to),
+        // "snippet": data.snippet,
+        // "starred": data.starred,
+        // "subject": data.subject,
+        "to": protectNull(data.to),
+        // "unread": data.unread,
+        "labels": protectNull(data.labels),
+        // "sync_category": data.sync_category,
+    }
+    console.info('message: ' + JSON.stringify(message));
+}
+
+protectNull = (list) => {
+    if (list = 'null') {
+        return null;
+    } else {
+        JSON.encode(list);
+    }
+}
+
 exports.snsNylasMessageHandler = async (event, context) => {
-    // All log statements are written to CloudWatch by default. For more information, see
-    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
-    console.info(event);
+    // const snsMessage = JSON.parse(event.Records[0].Sns.Message);
+    const snsMessage = JSON.parse(event.Message);
+
+    switch (event.MessageAttributes.type.Value) {
+        case 'com.nylas.messages.create.inflated':
+            handleEmail(snsMessage);
+            break;
+        default:
+            break;
+    }
 }
